b040916bd84652ff3333178a7e96b8b8d837d793
Brian Donahue <brimwd@gmail.com>
2017-08-29 18:04:54 -0700

    updates dist

--- dist/assets/fabricator/scripts/f.js
+++ dist/assets/fabricator/scripts/f.js
@@ -364,6 +364,7 @@
 	fabricator.setInitialMenuState().menuToggle().allItemsToggles().singleItemToggle().buildColorChips().setActiveItem().bindCodeAutoSelect();
 	
 	fillAllCssBlocks();
+	addVariations();
 	addVariationTags();
 	
 	/**
@@ -11409,13 +11410,34 @@
 	__webpack_require__(2);
 	var $ = __webpack_require__(1);
 	
+	window.addVariations = function () {
+		//dealing with each component's variations block
+		$('.f-item-variations').each(function (i, element) {
+			var preview = $(element).parents('.componentClasses').siblings('.f-item-preview');
+			var baseElement = preview.children()[0];
+	
+			var classes = element.textContent.split(',').map(function (className) {
+				//do something
+				className = className.split('');
+				return className.slice(className.indexOf('.') + 1, className.length).join('');
+			});
+			classes.pop(); //removes empty element at end of array
+	
+			classes.forEach(function (className) {
+				preview.append($(baseElement).clone().addClass(className));
+			});
+	
+			return;
+		});
+	};
+	
 	window.addVariationTags = function () {
-	  // find all elements with CSS 'front-matter'
-	  var elements = $('.f-item-variations').siblings('.f-item-preview').children();
-	  elements.each(function (i, element) {
-	    $(element).attr('data-variation', 'true');
-	    return element;
-	  });
+		// find all elements with CSS 'front-matter'
+		var elements = $('.f-item-variations').parents('.componentClasses').siblings('.f-item-preview').children();
+		elements.each(function (i, element) {
+			$(element).attr('data-variation', 'true');
+			return element;
+		});
 	};
 
 /***/ }),
