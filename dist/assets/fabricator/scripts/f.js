5b7979c4a785664c4d8711ecbf068d0ee8d4bbd6
Brian Donahue <brimwd@gmail.com>
2017-08-30 13:03:16 -0700

    updates dist

--- dist/assets/fabricator/scripts/f.js
+++ dist/assets/fabricator/scripts/f.js
@@ -11411,33 +11411,34 @@
 	var $ = __webpack_require__(1);
 	
 	window.addVariations = function () {
-			//dealing with each component's variations block
-			$('.f-item-variations').each(function (i, element) {
-					var preview = $(element).parents('.componentClasses').siblings('.f-item-preview');
-					var baseElement = preview.children()[0];
+		//dealing with each component's variations block
+		$('.f-item-variations').each(function (i, element) {
+			var preview = $(element).parents('.componentClasses').siblings('.f-item-preview');
+			var baseElement = preview.children()[0];
 	
-					var classes = element.textContent.split(' ').map(function (className) {
-							//do something
-							className = className.split('');
-							return className.slice(className.indexOf('.') + 1, className.length).join('');
-					});
-					classes.shift(); //removes empty string at the beginning of the array
+			var classes = element.textContent.split(' ').map(function (className) {
+				//do something
+				className = className.split('');
+				return className.slice(className.indexOf('.') + 1, className.length).join('');
+			}); //first array element is empty string - generates 'base' rendering
 	
-					classes.forEach(function (className) {
-							preview.append($(baseElement).clone().addClass(className));
-					});
+			preview.html(''); //clears contents of preview section
 	
-					return;
+			classes.forEach(function (className) {
+				preview.append($(baseElement).clone().addClass(className));
 			});
+	
+			return;
+		});
 	};
 	
 	window.addVariationTags = function () {
-			// find all elements with CSS 'front-matter'
-			var elements = $('.f-item-variations').parents('.componentClasses').siblings('.f-item-preview').children();
-			elements.each(function (i, element) {
-					$(element).attr('data-variation', 'true');
-					return element;
-			});
+		// find all elements with CSS 'front-matter'
+		var elements = $('.f-item-variations').parents('.componentClasses').siblings('.f-item-preview').children();
+		elements.each(function (i, element) {
+			$(element).attr('data-variation', 'true');
+			return element;
+		});
 	};
 
 /***/ }),
