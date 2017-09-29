b38efd4c3d7ac155b4613479024eeee92126c3d6
Brian Donahue <brimwd@gmail.com>
2017-09-29 15:10:27 -0700

    updates notes

--- src/assets/fabricator/scripts/getCSS.js
+++ src/assets/fabricator/scripts/getCSS.js
@@ -165,7 +165,8 @@ window.writeToBody = function (jqueryElement) {
            )
          );
 
-  // filter out "meaningless" properties that don't affect marketing, design, or desktop development
+  // Filter out "meaningless" properties that don't affect marketing, design, or desktop development
+  // TODO: These two loops could probably be combined into one loop. That might be fun for a dev to figure out...
   propertiesToFilter.forEach(function(property, i){
     delete unorderedProperties[property];
   });
@@ -190,7 +191,7 @@ window.writeToBody = function (jqueryElement) {
     // TODO: Multiply out rem values to px values
     // 1. If there's an instance of 'rem' in orderedProperties[property]
     // 2. Slice to orderedProperties[property].indexof('rem')
-    // 3. Multiply sliced value by 16
+    // 3. Multiply sliced value by 16, our base font-size
     // 4. Add 'px' to the string
     // 5. Assign the result to orderedProperties[property]
 
